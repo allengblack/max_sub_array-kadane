d819cea82092f019f863d1dac5498af2143e368a
allengblack <allengblack@gmail.com>
2017-05-14 11:36:52 +0100

    "add second Python3 version of sub array program with specified length"

--- sub_arr.py
+++ sub_arr.py
@@ -1,22 +1,23 @@
 
-def max_sub_arr(arr):
+def max_sub_array(arr, length):
     '''function to return the largest sub-array of given size with largest sum'''
-    max_sum = current_sum = 0
-    current_sub_arr_index = max_sub_arr_start_index = max_sub_arr_end_index = 0
-    for index, item in enumerate(arr):
-        if current_sum + item > 0:
-            current_sum += item
-        else:
-            current_sum = 0
-            current_sub_arr_index = index + 1
+    if length < 1:
+        return "Sub-array length must be at least 1"
+    if length == 1:
+        return [max(arr)]
+    if len(arr) == 0 or len(arr) == 1:
+        return "Array is empty or has only one item"
+    if len(arr) < length:
+        return "Required sub-array length greater than array {}".format(arr)
 
-        if current_sum > max_sum:
-            max_sub_arr_start_index = current_sub_arr_index
-            max_sub_arr_end_index = index + 1
-            max_sum = current_sum
-    return "The largest sub-array is {} and its sum is {}".format(
-        arr[max_sub_arr_start_index:max_sub_arr_end_index], max_sum)
+    max_sub_arr = []
+    max_sum = 0
 
-print(max_sub_arr([-2, 3, 2, -1]))
-print(max_sub_arr([-1, 2, -3, 4, -5]))
-print(max_sub_arr([4, -2, -8, 5, -2, 7, 7, 2, -6, 5]))
+    for i in range(len(arr) - length+1):
+        sub_arr = arr[i:i+length]
+        my_sum = sum(sub_arr)
+
+        if my_sum > max_sum:
+            max_sum = my_sum
+            max_sub_arr = sub_arr
+    return 'The max sum is {} from sub-array {}'.format(max_sum, max_sub_arr)
